bb1abd3a33cabb60c432a606da73ba239cad49f3
xatier <xatierlike@gmail.com>
2020-04-19 15:18:44 -0700

    Update query URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ chrome.contextMenus.create({
     "title": "Lookup \"%s\" on lexico.com",
     "contexts": ['selection'],
     "onclick": function (info, tab) {
-        var url = 'https://www.lexico.com/search?utf8=%E2%9C%93&filter=dictionary&dictionary=en&query='
+        var url = 'https://www.lexico.com/search?utf8=%E2%9C%93&filter=noad&dictionary=en&query='
             + encodeURIComponent(info.selectionText)
         chrome.tabs.create({ url : url });
     }
