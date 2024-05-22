ea5627c290f519baa923290cb2b0a76fa07fc677
xatier <xatierlike@gmail.com>
2024-05-22 12:12:48 -0500

    feat: look up in goo 辞書

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ chrome.contextMenus.create({
   title: 'Lookup "%s" on dictionary.com',
   contexts: ["selection"],
   onclick: function (info, tab) {
-    var url = `https://www.dictionary.com/browse/${encodeURIComponent(
+    const url = `https://www.dictionary.com/browse/${encodeURIComponent(
       info.selectionText
     )}`
+    chrome.tabs.create({ url: url, index: tab.index + 1 })
+  },
+})
+
+chrome.contextMenus.create({
+  title: 'Lookup "%s" on goo 辞書',
+  contexts: ["selection"],
+  onclick: function (info, tab) {
+    const url = `https://dictionary.goo.ne.jp/freewordsearcher.html?MT=${encodeURIComponent(info.selectionText)}&mode=0&kind=all`
     chrome.tabs.create({ url: url, index: tab.index + 1 })
   },
 })
