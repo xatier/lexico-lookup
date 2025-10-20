fcffe2d44a70d94b8ace3fa023e8bb920552e530
xatier <xatierlike@gmail.com>
2025-10-19 20:20:10 -0500

    feat(goo): remove goo 辞書 as service EOL

--- app.js
+++ app.js
@@ -9,12 +9,3 @@ chrome.contextMenus.create({
     chrome.tabs.create({ url: url, index: tab.index + 1 })
   },
 })
-
-chrome.contextMenus.create({
-  title: 'Lookup "%s" on goo 辞書',
-  contexts: ["selection"],
-  onclick: function (info, tab) {
-    const url = `https://dictionary.goo.ne.jp/freewordsearcher.html?MT=${encodeURIComponent(info.selectionText)}&mode=0&kind=all`
-    chrome.tabs.create({ url: url, index: tab.index + 1 })
-  },
-})
