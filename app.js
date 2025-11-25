2f555fa4c17ca5c99f7c0a92b2c7f5debb07f40c
xatier <xatierlike@gmail.com>
2025-11-25 17:34:47 -0600

    feat: look up in weblio 辞書

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ chrome.contextMenus.create({
     chrome.tabs.create({ url: url, index: tab.index + 1 })
   },
 })
+
+
+chrome.contextMenus.create({
+  title: 'Lookup "%s" on weblio 辞書',
+  contexts: ["selection"],
+  onclick: function (info, tab) {
+    const url = `https://www.weblio.jp/content_find?query=${encodeURIComponent(info.selectionText)}&searchType=exact`
+    chrome.tabs.create({ url: url, index: tab.index + 1 })
+  },
+})
