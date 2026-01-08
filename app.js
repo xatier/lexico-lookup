51e5d2fbeaa95aa9eddc6fb7133045acf5ee25bb
xatier <xatierlike@gmail.com>
2026-01-08 03:45:56 -0600

    feat: upgrade to manifest_version 3

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 /* eslint-disable no-unused-vars */
 chrome.contextMenus.create({
+  id: `dictionary-lookup`,
   title: 'Lookup "%s" on dictionary.com',
   contexts: ["selection"],
-  onclick: function (info, tab) {
+})
+
+/* eslint-disable no-unused-vars */
+chrome.contextMenus.create({
+  id: `weblio-lookup`,
+  title: 'Lookup "%s" on weblio 辞書',
+  contexts: ["selection"],
+})
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "dictionary-lookup") {
     const url = `https://www.dictionary.com/browse/${encodeURIComponent(
       info.selectionText
     )}`
     chrome.tabs.create({ url: url, index: tab.index + 1 })
-  },
-})
-
-
-chrome.contextMenus.create({
-  title: 'Lookup "%s" on weblio 辞書',
-  contexts: ["selection"],
-  onclick: function (info, tab) {
-    const url = `https://www.weblio.jp/content_find?query=${encodeURIComponent(info.selectionText)}&searchType=exact`
+  } else if (info.menuItemId === "weblio-lookup") {
+    const url = `https://www.weblio.jp/content_find?query=${encodeURIComponent(
+      info.selectionText
+    )}&searchType=exact`
     chrome.tabs.create({ url: url, index: tab.index + 1 })
-  },
+  } else {
+    // pass
+  }
 })
